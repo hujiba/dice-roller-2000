10b0da49534eead4715cc92b6b15a2f4c01a80fc
Owen Schafer <owcschafer@hotmail.com>
2020-11-21 05:17:53 +0000

    i honestly dont remember what changed

--- dice.py
+++ dice.py
@@ -8,7 +8,7 @@ from sys import platform
 if platform == "win32":
   init(convert=True)
 
-colors = ["RED", "YELLOW", "BLUE", "CYAN", "GREEN", "MAGENTA", "RED", "YELLOW", "BLUE", "CYAN", "GREEN", "MAGENTA", "RED", "YELLOW", "BLUE", "CYAN", "GREEN", "MAGENTA", "RED", "YELLOW", "BLUE", "CYAN", "GREEN", "MAGENTA", "RED", "YELLOW", "BLUE", "CYAN", "GREEN", "MAGENTA"]
+colors = ["RED", "YELLOW", "BLUE", "CYAN", "GREEN", "MAGENTA", "RED", "YELLOW", "BLUE", "CYAN", "GREEN", "MAGENTA", "RED", "YELLOW", "BLUE", "CYAN", "GREEN", "MAGENTA", "RED", "YELLOW", "BLUE", "CYAN", "GREEN", "MAGENTA"]
 
 sides = int(input("How many sides should the dice have?\n"))
 if sides == 0:
@@ -19,16 +19,17 @@ elif sides > 999999999:
   no = text2art("please no",font="larry3d")
   print(getattr(Fore, colors[randint(0, 30)]) + no)
 else:
-    for loop1 in range(30):
+    for loop1 in range(24):
       seed()
-      rolled = str(randint(1, sides))
+      rolled = str(randint(69, sides))
       art = text2art("  " + rolled,font='block',chr_ignore=True) # Return ASCII text with block font
       for loop2 in range(13):
         print ("\033[A                                                                        \033[A")
       color = colors[loop1]
       print(getattr(Fore, color) + Style.BRIGHT + art)
-      spin = loop1 * loop1 * 0.0003 + 0.05
+      spin = loop1 * loop1 * 0.0011 + 0.05
       time.sleep(spin)
+    time.sleep(0.3)
     for loop4 in range(2):
       for loop3 in range(14):
         print ("\033[A                                                                        \033[A")
