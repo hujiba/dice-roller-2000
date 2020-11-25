c22d8bc54c7b65f43091222644bae8b4fdb387f6
Owen Schafer <owcschafer@hotmail.com>
2020-11-25 00:10:18 +0000

    made it loop

--- dice.py
+++ dice.py
@@ -9,51 +9,53 @@ import funfacts
 if platform == "win32":
   init(convert=True) # idk it just breaks on windows if you dont have it and breaks on linux if you do
 
+loop = 1
 colors = ["RED", "YELLOW", "BLUE", "CYAN", "GREEN", "MAGENTA", "RED", "YELLOW", "BLUE", "CYAN", "GREEN", "MAGENTA", "RED", "YELLOW", "BLUE", "CYAN", "GREEN", "MAGENTA", "RED", "YELLOW", "BLUE", "CYAN", "GREEN", "MAGENTA"]
 
-try:
-  sides = int(input("How many sides should the dice have?\n"))
-  if sides == 0:
-    what = text2art("what",font="larry3d")
-    print(getattr(Fore, colors[randint(0, 5)]) + what)
-  elif sides > 999999999:
-    no = text2art("please no",font="larry3d")
-    print(getattr(Fore, colors[randint(0, 5)]) + no)
-  else:
-      for loop5 in range(7):
-        print("\n")
-      for loop1 in range(randint(20, 24)):
-        seed()
-        rolled = str(randint(1, sides))
-        art = text2art("  " + rolled,font='block',chr_ignore=True)
-        for loop2 in range(13):
-          print ("\033[A                                                                                       \033[A")
-        color = colors[loop1]
-        print(getattr(Fore, color) + Style.BRIGHT + art)
-        spin = loop1 * loop1 * 0.0011 + 0.05
-        time.sleep(spin)
-      time.sleep(0.3)
-      for loop4 in range(2):
+while loop == 1:
+  try:
+    sides = int(input("How many sides should the dice have?\n"))
+    if sides == 0:
+      what = text2art("what",font="larry3d")
+      print(getattr(Fore, colors[randint(0, 5)]) + what)
+    elif sides > 999999999:
+      no = text2art("please no",font="larry3d")
+      print(getattr(Fore, colors[randint(0, 5)]) + no)
+    else:
+        for loop5 in range(7):
+          print("\n")
+        for loop1 in range(randint(20, 24)):
+          seed()
+          rolled = str(randint(1, sides))
+          art = text2art("  " + rolled,font='block',chr_ignore=True)
+          for loop2 in range(13):
+            print ("\033[A                                                                                       \033[A")
+          color = colors[loop1]
+          print(getattr(Fore, color) + Style.BRIGHT + art)
+          spin = loop1 * loop1 * 0.0011 + 0.05
+          time.sleep(spin)
+        time.sleep(0.3)
+        for loop4 in range(2):
+          for loop3 in range(14):
+            print ("\033[A                                                                                       \033[A")
+          print(Fore.WHITE + Style.BRIGHT + "You rolled a:\n" + art)
+          time.sleep(0.5)
+          for loop3 in range(14):
+            print ("\033[A                                                                                       \033[A")
+          print(Fore.BLACK + Style.BRIGHT + "You rolled a:\n" + art)
+          time.sleep(0.5)
         for loop3 in range(14):
           print ("\033[A                                                                                       \033[A")
         print(Fore.WHITE + Style.BRIGHT + "You rolled a:\n" + art)
         time.sleep(0.5)
-        for loop3 in range(14):
-          print ("\033[A                                                                                       \033[A")
-        print(Fore.BLACK + Style.BRIGHT + "You rolled a:\n" + art)
-        time.sleep(0.5)
-      for loop3 in range(14):
-        print ("\033[A                                                                                       \033[A")
-      print(Fore.WHITE + Style.BRIGHT + "You rolled a:\n" + art)
-      time.sleep(0.5)
 
-      randFact = randint(0, 2)
+        randFact = randint(0, 2)
 
-      try:
-        numFacts = funfacts.funfacts[int(rolled)]
-        fact = numFacts[randFact]
-        print("Fun fact: " + fact)
-      except KeyError:
-        pass
-except:
-  print("Please enter a number!")
+        try:
+          numFacts = funfacts.funfacts[int(rolled)]
+          fact = numFacts[randFact]
+          print("Fun fact: " + fact + "\n")
+        except KeyError:
+          pass
+  except:
+    print("Please enter a number!\n")
